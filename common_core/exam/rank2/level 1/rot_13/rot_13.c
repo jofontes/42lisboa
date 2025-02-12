ca90182dd57fd4d1cd80606f742a2107eda01142
Joao Saraiva Fontes <josaraiv@c2r4s1.42lisboa.com>
2025-02-12 14:14:55 +0000

    .

--- common_core/exam/rank2/level 1/rot_13/rot_13.c
+++ common_core/exam/rank2/level 1/rot_13/rot_13.c
@@ -1,6 +1,46 @@
 #include <unistd.h>
 
+int main(int ac, char **av)
+{
+	if (ac == 2)
+	{
+		int i = 0;
+		int j;
+		char temp;
+		while (av[1][i])
+		{
+			temp = av[1][i];
+			j = 0;
+			if ((temp >= 'a' && temp <= 'z') || (temp >= 'A' && temp <= 'Z'))
+			{
+				while (j < 13)
+				{
+					if (temp == 'z')
+					{
+						temp = 'a';
+						j++;
+					}
+					else if (temp == 'Z')
+					{
+						temp = 'A';
+						j++;
+					}
+					else
+					{
+						temp = temp + 1;
+						j++;
+					}
+					
+				}
+			}	
+			write(1, &temp, 1);
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+}
 
+/*
 int main(int ac, char **av)
 {
 	int i;
@@ -28,4 +68,6 @@ int main(int ac, char **av)
 	}
 	write(1, "\n", 1);
 }
+*/
+
 
